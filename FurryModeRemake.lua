53185bc0e1db1c6d0cf85daf2c8117eb85191fcf
Anctarus7105 <idzhabkin@gmail.com>
2025-12-27 20:43:10 +0400

    Update and rename Furry Mode to FurryModeRemake.lua

---
+++ FurryModeRemake.lua
@@ -0,0 +1,40 @@
+--[[
+    _               _                      _____ _  ___  ____  
+   / \   _ __   ___| |_ __ _ _ __ _   _ __|___  / |/ _ \| ___| 
+  / _ \ | '_ \ / __| __/ _` | '__| | | / __| / /| | | | |___ \ 
+ / ___ \| | | | (__| || (_| | |  | |_| \__ \/ / | | |_| |___) |
+/_/   \_\_| |_|\___|\__\__,_|_|   \__,_|___/_/  |_|\___/|____/ 
+
+-- Join my discord server for new scripts , releases , sneak peaks!
+-- https://discord.gg/2x5NGVya
+-- Credits: TheendisnearDonotfear , Oof , Thanhtv68
+--]]
+
+-- Oof's Furry Mode
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Thanhtv68/DoorsMode/refs/heads/main/But%20Furry"))()
+
+-- Furry Ambush
+coroutine.wrap(function()
+    while true do 
+        wait(math.random(80, 110))
+         game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(1.5)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Voor-Pr00/spawnnn/refs/heads/main/WH1T3_Obfuscate"))()
+end
+end)()
+
+-- Furry Rush
+coroutine.wrap(function()
+    while true do 
+        wait(math.random(100, 130))
+         game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(0)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Voor-Pr00/spawnnn/refs/heads/main/SMILER_Obfuscate"))()
+end
+end)()
+
+--[[
+Furry Rush Chance = 85%
+Furry Ambush Chance = 55%
+Furry Entity Chance = ?%
+--]]
