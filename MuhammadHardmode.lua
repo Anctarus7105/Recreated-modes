0af157fa62e04631dfdbcc3a423a2b7234dc6da5
Anctarus7105 <idzhabkin@gmail.com>
2026-01-14 19:47:16 +0400

    Create MuhammadHardmode.lua

---
+++ MuhammadHardmode.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/localplayerr/Doors-stuff/refs/heads/main/Hardcore%20v4%20recreate/main%20code"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Anctarus7105/Random-Files/refs/heads/main/Hardmode%20Chat%20Meaasge"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Anctarus7105/Random-Files/refs/heads/main/GuidingLightHardCore"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Anctarus7105/Random-Files/refs/heads/main/HardcoreModeSounds.lua"))()
